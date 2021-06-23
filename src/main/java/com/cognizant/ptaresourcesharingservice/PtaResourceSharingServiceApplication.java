9f56e52bb5e7e15fff7f5c5cb4603032f72af74b
Suresh Kumar <suresh.uncbc.coding@gmail.com>
2021-06-22 23:34:24 -0400

    Initial Commit

---
+++ src/main/java/com/cognizant/ptaresourcesharingservice/PtaResourceSharingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.ptaresourcesharingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtaResourceSharingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtaResourceSharingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cognizant/ptaresourcesharingservice/PtaResourceSharingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.ptaresourcesharingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtaResourceSharingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
